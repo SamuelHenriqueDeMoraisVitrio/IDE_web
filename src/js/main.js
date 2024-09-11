a6616e66624978f3caa7e9220e80b6e9d4139c68
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-09-11 19:44:44 -0300

    por class

--- src/js/main.js
+++ src/js/main.js
@@ -1,63 +1,71 @@
-const editor = ace.edit("editor");
+const editors = document.querySelectorAll('.ace-editor');
+const aceEditors = [];
 
-editor.session.setMode("ace/mode/lua");
+editors.forEach((element, index) => {
+    const editor = ace.edit(element.id);
 
-editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/lua");
+    editor.setTheme("ace/theme/monokai");
+    editor.setOptions(CONFIG_EDITOR);
 
-editor.setOptions(CONFIG_EDITOR);
+    editor.setValue(INIT_CODE, CURSOR_INIT_LINE);
+
+    editor.session.on('change', function() {
+        updateScope();
+    });
+
+    aceEditors.push(editor);
+});
 
 const scope = {
-  functions: {},
-  variables: {}
+    functions: {},
+    variables: {}
 };
 
-editor.setValue(INIT_CODE, CURSOR_INIT_LINE);
-
 function updateScope() {
-  const code = editor.getValue();
+    aceEditors.forEach(editor => {
+        const code = editor.getValue();
 
-  const functionMatches = code.match(/function\s+(\w+)/g) || [];
-  const variableMatches = code.match(/(?:local\s+|[\w]+\s*=\s*)(\w+)/g) || [];
+        const functionMatches = code.match(/function\s+(\w+)/g) || [];
+        const variableMatches = code.match(/(?:local\s+|[\w]+\s*=\s*)(\w+)/g) || [];
 
-  scope.functions = {};
-  scope.variables = {};
+        scope.functions = {};
+        scope.variables = {};
 
-  functionMatches.forEach(match => {
-    const functionName = match.split(/\s+/)[1];
-    scope.functions[functionName] = functionName;
-  });
+        functionMatches.forEach(match => {
+            const functionName = match.split(/\s+/)[1];
+            scope.functions[functionName] = functionName;
+        });
 
-  variableMatches.forEach(match => {
-    const variableName = match.split(/\s+/)[1] || match;
-    scope.variables[variableName] = variableName;
-  });
+        variableMatches.forEach(match => {
+            const variableName = match.split(/\s+/)[1] || match;
+            scope.variables[variableName] = variableName;
+        });
+    });
 }
 
 const customCompleter = {
-  getCompletions: function(editor, session, pos, prefix, callback) {
-    updateScope();
+    getCompletions: function(editor, session, pos, prefix, callback) {
+        updateScope();
 
-    const suggestions = [
-      ...Object.keys(scope.functions).map(name => ({
-        caption: name,
-        value: name + "()",
-        meta: "Função Lua"
-      })),
-      ...Object.keys(scope.variables).map(name => ({
-        caption: name,
-        value: name,
-        meta: "Variável Lua"
-      })),
-      ...DEFAULT_SUGGESTIONS
-    ];
-    
-    callback(null, suggestions);
-  }
+        const suggestions = [
+            ...Object.keys(scope.functions).map(name => ({
+                caption: name,
+                value: name + "()",
+                meta: "Função Lua"
+            })),
+            ...Object.keys(scope.variables).map(name => ({
+                caption: name,
+                value: name,
+                meta: "Variável Lua"
+            })),
+            ...DEFAULT_SUGGESTIONS
+        ];
+        
+        callback(null, suggestions);
+    }
 };
 
-editor.completers = [customCompleter];
-
-editor.session.on('change', function() {
-  updateScope();
+aceEditors.forEach(editor => {
+    editor.completers = [customCompleter];
 });
-
